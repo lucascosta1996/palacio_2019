f0ed754fb9856750b033d7f386a9d092e8d84a1b
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-05-05 22:56:01 -0300

    Create ViewingRoomIndex component

---
+++ src/Components/ViewingRoom/ViewingRoomIndex.js
@@ -0,0 +1,108 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { estadioWorks } from './estadioWorks';
+import { selectedWorks } from '../Catalogue/selectedWorks';
+import styled from 'styled-components';
+import { I18nContext } from '../../i18n';
+
+const ViewingRoomIndexWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+
+  .exhibition {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    padding-top: 150px;
+    text-decoration: none;
+
+    .imageWrapper {
+      img {
+        max-width: 500px;
+      }
+    }
+
+    .marginRight {
+      margin-right: 5px;
+    }
+
+    .infosCurrentEx {
+      display: flex;
+      align-self: flex-start;
+
+      span {
+        color: #000;
+        display: block;
+        font-size: 13px;
+        padding: 5px 0;
+        text-decoration: none;
+      }
+    }
+  }
+`
+
+function ViewingRoomIndex () {
+  const { translate } = useContext(I18nContext)
+
+  return (
+    <ViewingRoomIndexWrapper>
+      <Link
+        className="exhibition"
+        to={{
+          pathname: "/viewing-room",
+          state: {
+            exhibition: {
+              name: 'Estádio',
+              date: 'estadioDate'
+            },
+            works: estadioWorks
+          }
+        }}
+      >
+        <div className="imageWrapper">
+          <img
+            alt="Galeria Palácio - Estádio - Emerson da Silva"
+            src={ require( `../../assets/index/Emerson-da-Silva-Estádio-2020-Galeria-Palácio-All-rights-reserved.jpg` ) }
+          />
+        </div>
+        <div className="infosCurrentEx">
+          <span className="marginRight">
+            Emerson da Silva: <i>Estádio</i>,
+          </span>
+          <span>
+            {translate('estadioDate')}
+          </span>
+        </div>
+      </Link>
+      <Link
+        className="exhibition"
+        to={{
+          pathname: "/viewing-room/selected-works",
+          state: {
+            exhibition: {
+              name: 'Selected Works',
+              date: 'selectedWorksDate'
+            },
+            works: selectedWorks
+          }
+        }}
+      >
+        <div className="imageWrapper">
+          <img alt="Galeria Palácio current exhibition - Contato visual, 2019 - Chiaki Mihara" src={ require( `../../assets/exhibitions/contatoVisual/Estudo-para-Contato-Visual-2019-Chiaki-Mihara-Installation-View-Galeria-Palácio-(2).jpg` ) } />
+        </div>
+        <div className="infosCurrentEx">
+          <span className="marginRight">
+            <i>Selected Works</i>,
+          </span>
+          <span>
+            {translate('selectedWorksDate')}
+          </span>
+        </div>
+      </Link>
+    </ViewingRoomIndexWrapper>
+  )
+}
+
+export default ViewingRoomIndex;
