dd463b0d6b38fbe96be2d20a250315dd08180ee6
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-05-05 22:56:25 -0300

    Create new works list for exhibition

---
+++ src/Components/ViewingRoom/estadioWorks.js
@@ -0,0 +1,20 @@
+export const estadioWorks = [
+  {
+    name: 'Autorretrato (2)',
+    period: '(2018)',
+    artist: 'Chiaki Mihara',
+    route: 'autoretrato',
+    coverImage: '4-Autorretrato-(2)-2018-Chiaki-Mihara-Installation-View-Photo-Galeria-Palácio-(4).jpg',
+    info1: 'autoretrato1',
+    info2: 'autoretrato2',
+    info3: 'autoretrato3',
+    text: 'autorretratoVR',
+    date: 'autoretrato1date',
+    slides: [
+      {
+        img: 'catalogue/autoretrato/Autorretrato-(2)-2018-Chiaki-Mihara-Installation-View-Galeria-Palácio-(4).jpg',
+        index: 0
+      }
+    ]
+  }
+]
