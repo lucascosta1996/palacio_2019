6e594e5a3ec54f7f4a95dc0892b73084f7b44bb3
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-05-06 22:07:39 -0300

    Add works list to OVR JSON

--- src/Components/Exhibitions/viewingRoomExhibitionsJson.js
+++ src/Components/Exhibitions/viewingRoomExhibitionsJson.js
@@ -3,15 +3,89 @@ export const viewingRoomExhibitions = [
     artist: 'Galeria Palácio',
     showName: 'Selected works',
     showDate: 'selectedWorksDate',
-    showRoute: 'viewing-room/selected-works',
+    showRoute: '/viewing-room/selected-works',
     period: 'current',
-    isNameHidden: true
+    isNameHidden: true,
+    works: [
+      {
+        name: 'Autorretrato (2)',
+        period: '(2018)',
+        artist: 'Chiaki Mihara',
+        route: 'autoretrato',
+        coverImage: '4-Autorretrato-(2)-2018-Chiaki-Mihara-Installation-View-Photo-Galeria-Palácio-(4).jpg',
+        info1: 'autoretrato1',
+        info2: 'autoretrato2',
+        info3: 'autoretrato3',
+        text: 'autorretratoVR',
+        date: 'autoretrato1date',
+        slides: [
+          {
+            img: 'catalogue/autoretrato/Autorretrato-(2)-2018-Chiaki-Mihara-Installation-View-Galeria-Palácio-(4).jpg',
+            index: 0
+          }
+        ]
+      },
+      {
+        name: 'Terrestre',
+        period: '(2017)',
+        artist: 'Emerson da Silva',
+        route: 'luzaguaterrapreta',
+        coverImage: '2-Terrestre-2017-Emerson-da-Silva-Screenshot-(3)-All-Rights-Reserved.jpg',
+        price: '$ 155,000',
+        info1: 'luzaguaterra1',
+        info2: 'luzaguaterra2',
+        text: 'terrestreVR',
+        date: 'luzaguaterradate',
+        slides: [
+          {
+            img: 'catalogue/2-Terrestre-2017-Emerson-da-Silva-Screenshot-(3)-All-Rights-Reserved.jpg',
+            index: 0
+          }
+        ]
+      },
+      {
+        name: 'Light Particles',
+        period: '(2017–2018)',
+        artist: 'Andrés Stephanou',
+        route: 'light-particles',
+        coverImage: 'Light-Particles-2017-2018-Andrés-Stephanou-Screenshot-(2)-All-rights-reserved.png',
+        info1: 'particulasdeluz1-1',
+        info2: 'particulasdeluz1-2',
+        text: 'lightVR',
+        date: 'particulasdeluz1date',
+        slides: [
+          {
+            img: 'catalogue/Light-Particles-2017-2018-Andrés-Stephanou-Screenshot-(2)-All-rights-reserved.png',
+            index: 0
+          }
+        ]
+      },
+      {
+        name: 'Terreno',
+        period: '(2019)',
+        artist: 'Lucas Alves Costa',
+        route: 'terreno',
+        coverImage: 'Terreno-2019-Lucas-Alves-Costa-All-rights-reserved-Galeria-Palácio-(1) (2).jpg',
+        info1: 'terreno1',
+        info2: 'terreno2',
+        info3: 'terreno3',
+        info4: 'terreno4',
+        text: 'terrenoVR',
+        date: 'terrenodate',
+        slides: [
+          {
+            img: 'catalogue/Terreno-2019-Lucas-Alves-Costa-All-rights-reserved-Galeria-Palácio-(1) (2).jpg',
+            index: 0
+          },
+        ]
+      }
+    ]
   },
   {
     artist: 'Emerson da Silva',
     showName: 'Estádio',
     showDate: 'estadioDate',
-    showRoute: 'estadio',
+    showRoute: '/viewing-room',
     period: 'upcoming',
     upcomingTitle: 'estadioUpcomingTitle',
     upcomingOpening: 'estadioOpening'
