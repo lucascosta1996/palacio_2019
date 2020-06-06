77bb596abaaf9854e59ce1fab83ed2e3da5c6768
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-06-05 23:26:07 -0300

    Prettify indentation and add conditional for event parameter

--- src/Components/Language/LanguageOptions.js
+++ src/Components/Language/LanguageOptions.js
@@ -1,12 +1,12 @@
-import React, { useContext } from "react";
-import styled from 'styled-components'
+import React, { useContext, useEffect } from "react";
+import styled from "styled-components";
 import { I18nContext } from "../../i18n";
 
 const OptionsWrapper = styled.div`
   width: 20px;
   z-index: 2;
 
-  @media ( max-width: 520px ) {
+  @media (max-width: 520px) {
     bottom: 45px;
     right: 35px;
     z-index: 5;
@@ -15,7 +15,7 @@ const OptionsWrapper = styled.div`
   span {
     color: #000;
     cursor: pointer;
-    font-family: 'Roboto', sans-serif;
+    font-family: "Roboto", sans-serif;
     font-size: 13px;
     font-weight: 300;
     text-transform: uppercase;
@@ -28,29 +28,26 @@ const OptionsWrapper = styled.div`
   .checked {
     font-weight: bold;
   }
-`
+`;
 
-const LanguageOptions = props => {
-  const { langCode, dispatch } = useContext(I18nContext)
+const LanguageOptions = (props) => {
+  const { langCode, dispatch } = useContext(I18nContext);
 
-  const onLanguageSelect = e => {
-    return dispatch({ type: "setLanguage", payload: e.target.innerHTML })
-  }
-  
-  const renderOption = code => (
-    <span
-      onClick={ ( code ) => onLanguageSelect( code ) }
-    >
-      {code}
-    </span>
-  )
+  const onLanguageSelect = (event) => {
+    const language = event.target ? event.target.innerHTML : event; 
+    return dispatch({ type: "setLanguage", payload: language});
+  };
+
+  const renderOption = (code) => (
+    <span onClick={(code) => onLanguageSelect(code)}>{code}</span>
+  );
 
   return (
     <OptionsWrapper>
-      {langCode === 'en' && renderOption("pt")}
-      {langCode === 'pt' && renderOption("en")}
+      {langCode === "en" && renderOption("pt")}
+      {langCode === "pt" && renderOption("en")}
     </OptionsWrapper>
-  )
-}
+  );
+};
 
 export default LanguageOptions;
