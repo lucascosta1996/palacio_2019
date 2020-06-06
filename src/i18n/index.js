c4ebd0a43bcdd865428ab0726d752576a3328e6f
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-06-05 23:28:11 -0300

    Request translation data from firestore collection

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,33 +1,19 @@
-import React, { useReducer } from "react"
-import EN from "./en.json"
-import PT from "./pt.json"
+import React, { useReducer, useEffect, useState } from "react";
+import firebase from "../firebase";
+import EN from "./en.json";
+import PT from "./pt.json";
 
-// To make it easier to read from JSON files
-let translations = {
-  en: EN,
-  pt: PT
-};
-
-const getTranslate = langCode => key => translations[langCode][key] || key;
-
-const initialState = {
-  langCode: "en",
-  translate: getTranslate("en"),
-}
-
-// const getEnglishTexts = async () => {
-//   let result;
-//   await firebase.getEnglishText().then( res => { return result = res } )
-//   translations.en = Object.assign( result, translations )
-//   console.log( translations )
-// }
-
-// console.log( getEnglishTexts() )
-
-export const I18nContext = React.createContext(initialState)
+export const I18nContext = React.createContext();
 
 export const I18nContextProvider = ({ children }) => {
-  
+  const getTranslate = (langCode) => (key) =>
+    translations[langCode][key] || key;
+
+  const initialState = {
+    langCode: "en",
+    translate: getTranslate("en"),
+  };
+
   const reducer = (state, action) => {
     switch (action.type) {
       case "setLanguage":
@@ -41,6 +27,16 @@ export const I18nContextProvider = ({ children }) => {
   };
 
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [translations, setTranslations] = useState({ en: EN, pt: PT });
+
+  useEffect(() => {
+    async function fetchData() {
+      const englishText = await firebase.getTextCollection( 'en' ).then((result) => result);
+      const portugueseText = await firebase.getTextCollection( 'pt' ).then((result) => result);
+      setTranslations({...translations, en: englishText, pt: portugueseText})
+    };
+    fetchData();
+  })
 
   return (
     <I18nContext.Provider value={{ ...state, dispatch }}>
